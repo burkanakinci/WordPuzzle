04ec7e8e27c05c076b05d46410dec2d8f454b73e
Burkan Akıncı <burkanakinci@gmail.com>
2023-03-31 06:58:03 +0300

    Added Main Area Tween

--- Assets/Scripts/UI/Areas/MainArea.cs
+++ Assets/Scripts/UI/Areas/MainArea.cs
@@ -9,20 +9,16 @@ public class MainArea : BaseArea<MainMenuPanel>
 
     [Header("On Tween Fields")]
     [SerializeField] private float m_LevelsButtonDelayedDuration;
-    [SerializeField] private float m_LevelsButtonGrowthDuration;
-    [SerializeField] private float m_LevelsButtonShrinkageDuration;
 
     public override void Initialize(MainMenuPanel _cachedComponent)
     {
         base.Initialize(_cachedComponent);
         m_LevelsButtonDelayedCallID = GetInstanceID() + "m_LevelsButtonDelayedCallID";
-        m_LevelsButtonSequenceID = GetInstanceID() + "m_LevelsButtonSequenceID";
+
         m_OpenLevelAreaButton.Initialize(this);
         HideArea();
     }
 
-    private string m_LevelsButtonSequenceID;
-    private Sequence m_LevelsButtonSequence;
     public override void ShowArea()
     {
         base.ShowArea();
@@ -35,26 +31,13 @@ public class MainArea : BaseArea<MainMenuPanel>
         DOTween.Kill(m_LevelsButtonDelayedCallID);
         DOVirtual.DelayedCall((m_LevelsButtonDelayedDuration), () =>
             {
-                LevelButtonSequence();
+                m_OpenLevelAreaButton.LevelButtonShowSequence();
             })
             .SetId(m_LevelsButtonDelayedCallID);
     }
-    private void LevelButtonSequence()
-    {
-        DOTween.Kill(m_LevelsButtonSequenceID);
-        m_LevelsButtonSequence = DOTween.Sequence().SetId(m_LevelsButtonSequenceID);
-        m_LevelsButtonSequence.Append(m_OpenLevelAreaButton.transform.DOScale((Vector3.one * 1.18f), m_LevelsButtonGrowthDuration).SetEase(Ease.OutExpo));
-        m_LevelsButtonSequence.Append(m_OpenLevelAreaButton.transform.DOScale((Vector3.one), m_LevelsButtonShrinkageDuration).SetEase(Ease.InExpo));
-        m_LevelsButtonSequence.AppendCallback(() =>
-        {
-            m_OpenLevelAreaButton.EnableLevelStartButton();
-        });
-    }
-
     private void KillAllTween()
     {
         DOTween.Kill(m_LevelsButtonDelayedCallID);
-        DOTween.Kill(m_LevelsButtonSequenceID);
     }
     public override void HideArea()
     {
--- Assets/Scripts/UI/Buttons/OpenLevelAreaButton.cs
+++ Assets/Scripts/UI/Buttons/OpenLevelAreaButton.cs
@@ -2,17 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class OpenLevelAreaButton : UIBaseButton<MainArea>
 {
+
+    [Header("On Tween Fields")]
+    [SerializeField] private float m_LevelsButtonGrowthDuration;
+    [SerializeField] private float m_LevelsButtonShrinkageDuration;
+
+    private string m_LevelsButtonScaleSequenceID;
+    private Sequence m_LevelsButtonShowSequence;
     public override void Initialize(MainArea _cachedComponent)
     {
         base.Initialize(_cachedComponent);
+        m_LevelButtonScaleTweenID = GetInstanceID() + "m_LevelButtonScaleTweenID";
+        m_LevelsButtonScaleSequenceID = GetInstanceID() + "m_LevelsButtonScaleSequenceID";
     }
 
     protected override void OnClickAction()
     {
-        CachedComponent.CachedComponent.HideAllArea();
-        CachedComponent.CachedComponent.ShowArea((int)MainMenuAreas.LevelPopupArea);
+        LevelButtonScaleTween((Vector3.zero), (m_LevelsButtonGrowthDuration), (Ease.OutExpo), (() =>
+        {
+            CachedComponent.CachedComponent.HideAllArea();
+            CachedComponent.CachedComponent.ShowArea((int)MainMenuAreas.LevelPopupArea);
+        }));
+    }
+    private string m_LevelButtonScaleTweenID;
+    private Tween LevelButtonScaleTween(Vector3 _scale, float _duration, Ease ease = Ease.Linear, TweenCallback _onCompleteCallback = null)
+    {
+        DOTween.Kill(m_LevelButtonScaleTweenID);
+        return transform.DOScale
+        ((_scale),
+        (m_LevelsButtonGrowthDuration))
+        .SetEase(Ease.OutExpo)
+        .SetId(m_LevelButtonScaleTweenID)
+        .OnComplete(() => _onCompleteCallback?.Invoke());
+    }
+    public void LevelButtonShowSequence()
+    {
+        DOTween.Kill(m_LevelsButtonScaleSequenceID);
+        m_LevelsButtonShowSequence = DOTween.Sequence().SetId(m_LevelsButtonScaleSequenceID);
+        m_LevelsButtonShowSequence.Append(LevelButtonScaleTween((Vector3.one * 1.18f), (m_LevelsButtonGrowthDuration), (Ease.OutExpo)));
+        m_LevelsButtonShowSequence.Append(LevelButtonScaleTween((Vector3.one), (m_LevelsButtonShrinkageDuration), (Ease.InExpo)));
+        m_LevelsButtonShowSequence.AppendCallback(() =>
+        {
+            EnableLevelStartButton();
+        });
+
+    }
+
+    private void KillAllTween()
+    {
+        DOTween.Kill(m_LevelButtonScaleTweenID);
+        DOTween.Kill(m_LevelsButtonScaleSequenceID);
+    }
+
+    public override void DisableLevelStartButton()
+    {
+        KillAllTween();
+        base.DisableLevelStartButton();
+    }
+
+    protected override void OnDestroy()
+    {
+        KillAllTween();
+        base.OnDestroy();
     }
 }
